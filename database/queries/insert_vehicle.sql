82c806dcfa21b17c6357eb4753e911a3311d2d70
Andrew Nida <nida.7@buckeyemail.osu.edu>
2025-01-24 06:09:34 -0500

    switching to quotes in sql queries

--- database/queries/insert_vehicle.sql
+++ database/queries/insert_vehicle.sql
@@ -2,31 +2,31 @@ START TRANSACTION;
 
 -- Insert into regions
 INSERT IGNORE INTO regions (uri, display)
-VALUES ('{region_uri}', '{region_display}');
+VALUES ("{region_uri}", "{region_display}");
 
 -- Get the last inserted ID (or fetch an existing one)
-SET @region_id = (SELECT id FROM regions WHERE uri = '{region_uri}');
+SET @region_id = (SELECT id FROM regions WHERE uri = "{region_uri}");
 
 -- Insert into makes
 INSERT IGNORE INTO makes (uri, display, path, region_id)
-VALUES ('{make_uri}', '{make_display}', '{make_path}', @region_id);
+VALUES ("{make_uri}", "{make_display}", "{make_path}", @region_id);
 
 -- Get the last inserted make ID (or fetch an existing one)
-SET @make_id = (SELECT id FROM makes WHERE uri = '{make_uri}');
+SET @make_id = (SELECT id FROM makes WHERE uri = "{make_uri}");
 
 -- Insert into models
 INSERT IGNORE INTO models (uri, display, make_id, path)
-VALUES ('{model_uri}', '{model_display}', @make_id, '{model_path}');
+VALUES ("{model_uri}", "{model_display}", @make_id, "{model_path}");
 
 -- Get the last inserted model ID (or fetch an existing one)
 SET @model_id = (SELECT id FROM models WHERE uri = '{model_uri}');
 
 -- Insert into frames
 INSERT IGNORE INTO frames (uri, display, model_id, path)
-VALUES ('{frame_uri}', '{frame_display}', @model_id, '{frame_path}');
+VALUES ("{frame_uri}", "{frame_display}", @model_id, "{frame_path}");
 
 -- Get the last inserted frame ID (or fetch an existing one)
-SET @frame_id = (SELECT id FROM frames WHERE uri = '{frame_uri}');
+SET @frame_id = (SELECT id FROM frames WHERE uri = "{frame_uri}");
 
 -- Insert into frame_nums
 INSERT IGNORE INTO frame_nums (num_from, num_to)
@@ -61,28 +61,28 @@ SET @body_style_id = (SELECT id FROM body_styles WHERE doors = {doors} LIMIT 1);
 
 -- Insert into transmissions
 INSERT IGNORE INTO transmissions (code, speeds, auto)
-VALUES ('{transmission_code}', {transmission_speeds}, {transmission_auto});
+VALUES ("{transmission_code}", {transmission_speeds}, {transmission_auto});
 
 -- Get the last inserted transmission ID (or fetch an existing one)
-SET @transmission_id = (SELECT id FROM transmissions WHERE code = '{transmission_code}');
+SET @transmission_id = (SELECT id FROM transmissions WHERE code = "{transmission_code}");
 
 -- Insert into trims
 INSERT IGNORE INTO trims (uri, display)
-VALUES ('{trim_uri}', '{trim_display}');
+VALUES ("{trim_uri}", "{trim_display}");
 
 -- Get the last inserted trim ID (or fetch an existing one)
-SET @trim_id = (SELECT id FROM trims WHERE uri = '{trim_uri}');
+SET @trim_id = (SELECT id FROM trims WHERE uri = "{trim_uri}");
 
 -- Insert into variants
 INSERT INTO variants (uri, display)
-SELECT '{variant_uri}', '{variant_display}'
-WHERE '{variant_uri}' != 'None' AND '{variant_display}' != 'None';
+SELECT "{variant_uri}", "{variant_display}"
+WHERE "{variant_uri}" != 'None' AND "{variant_display}" != 'None';
 
 -- Get the last inserted variant ID (or fetch an existing one)
-SET @variant_id = (SELECT id FROM variants WHERE uri = '{variant_uri}');
+SET @variant_id = (SELECT id FROM variants WHERE uri = "{variant_uri}");
 
 -- Insert into vehicles
 INSERT IGNORE INTO vehicles (frame_id, frame_nums_id, year_id, body_style_id, trim_id, variant_id, transmission_id, path)
-VALUES (@frame_id, @frame_nums_id, @year_id, @body_style_id, @trim_id, @variant_id, @transmission_id, '{vehicle_path}');
+VALUES (@frame_id, @frame_nums_id, @year_id, @body_style_id, @trim_id, @variant_id, @transmission_id, "{vehicle_path}");
 
 COMMIT
